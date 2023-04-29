34497a27a106b380833f94583c9be67780733543
mahesh-patil20 <104311917+mahesh-patil20@users.noreply.github.com>
2023-04-29 17:29:18 +0530

    Donate, Receiver,DonationForm pages added and given to sahil for updation

--- components/Donate.jsx
+++ components/Donate.jsx
@@ -1,10 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState , TouchableOpacity } from 'react';
+import { useNavigation, Button } from '@react-navigation/native';
 
 export const Donate =  () => {
+    const navigation = useNavigation();
+
+    const handleButtonPress = () => {
+        navigation.navigate('DonationForm');
+    };
 
     return(
         <View>
-            Hello
+            <Button title="Donate" onPress={handleButtonPress} />
+            <Text>Your Donation History</Text>
+            <View style={styles.container}>
+                <FlatList
+                    data={[
+                        {key: 'Devin'},
+                    ]}
+                    renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+                />
+            </View>
         </View>
     );
 }
