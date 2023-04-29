d0af7bd270b26feb452780fa8f75e556bc1b8efc
mahesh-patil20 <patilmaheshmp20@gmail.com>
2023-04-30 00:30:59 +0530

    Donation history implemented

--- components/Donate.jsx
+++ components/Donate.jsx
@@ -1,26 +1,45 @@
-import React, { useState , TouchableOpacity } from 'react';
-import { useNavigation} from '@react-navigation/native';
-import {FlatList, View, Text , Button} from "react-native";
+import React, {useEffect, useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {Button, Text, View} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {createKeyboardAwareNavigator} from "react-navigation";
 
 export const Donate =  () => {
     const navigation = useNavigation();
 
-    const handleButtonPress = () => {
-        navigation.navigate('DonationForm');
-    };
+    const [items , setItems] = useState([])
+
+    useEffect(()=>{
+        const formData = new FormData();
+        async function getToken(){
+            formData.append('donor_token' , await AsyncStorage.getItem("@donor_token"));
+        }
+            getToken().then(() =>
+            fetch('http://192.168.3.198:8000/myItems', {
+                method: 'POST',
+                body: formData,
+            })).then(res => {
+                if (res.status === 200) {
+                    console.log("Success")
+                    return res.json()
+                }
+            }).then(
+                data => {
+                    setItems(data.items);
+                }
+            )
+    })
 
     return(
         <View>
-            <Button title="Donate" onPress={handleButtonPress} />
+            <Button title="Donate" onPress={e => navigation.navigate('DonationForm')} />
             <Text>Your Donation History</Text>
             <View>
-                <Text>Test</Text>
-                {/*<FlatList*/}
-                {/*    data={[*/}
-                {/*        {key: 'Devin'},*/}
-                {/*    ]}*/}
-                {/*    renderItem={({item}) => <Text>{item.key}</Text>}*/}
-                {/*/>*/}
+            {items.map((item , index) => (
+                <View>
+                   <Text>{item.name}</Text>
+                </View>
+            ))}
             </View>
         </View>
     );
