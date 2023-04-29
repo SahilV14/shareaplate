12028f8e72103e27b9d8613fb6ab3f9946dc28a3
mahesh-patil20 <104311917+mahesh-patil20@users.noreply.github.com>
2023-04-29 11:55:43 +0530

    new login page first commit

--- components/Homepage.jsx
+++ components/Homepage.jsx
@@ -1,22 +1,6 @@
 import React, { useState } from 'react';
-import {
-    SafeAreaView,
-    ScrollView,
-    StatusBar,
-    StyleSheet,
-    Text,
-    useColorScheme,
-    View,
-    TextInput,
-    TouchableOpacity,
-} from 'react-native';
-import {
-    Colors,
-    DebugInstructions,
-    Header,
-    LearnMoreLinks,
-    ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {TextComponent} from "react-native";
+
 const Homepage =  () => {
     const onPressLogin = () => {
 // Do something about login operation
@@ -32,78 +16,16 @@ const Homepage =  () => {
         password: '',
     })
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}> Login Screen</Text>
-            <View style={styles.inputView}>
-                <TextInput
-                    style={styles.inputText}
-                    placeholder="Email"
-                    placeholderTextColor="#003f5c"
-                    onChangeText={text => setState({email:text})}/>
-            </View>
-            <View style={styles.inputView}>
-                <TextInput
-                    style={styles.inputText}
-                    secureTextEntry
-                    placeholder="Password"
-                    placeholderTextColor="#003f5c"
-                    onChangeText={text => setState({password:text})}/>
-            </View>
-            <TouchableOpacity
-                onPress = {onPressForgotPassword}>
-                <Text style={styles.forgotAndSignUpText}>Forgot Password?</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                onPress = {onPressLogin}
-                style={styles.loginBtn}>
-                <Text style={styles.loginText}>LOGIN </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                onPress = {onPressSignUp}>
-                <Text style={styles.forgotAndSignUpText}>Signup</Text>
-            </TouchableOpacity>
-        </View>
+
+        <TextComponent style={styles.heading}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At ducimus fugit illo iure maxime nobis odit officia quisquam tempore ut?</TextComponent>
+
     );
 }
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#4FD3DA',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    title:{
-        fontWeight: "bold",
-        fontSize:50,
-        color:"#fb5b5a",
-        marginBottom: 40,
-    },
-    inputView:{
-        width:"80%",
-        backgroundColor:"#3AB4BA",
-        borderRadius:25,
-        height:50,
-        marginBottom:20,
-        justifyContent:"center",
-        padding:20
-    },
-    inputText:{
-        height:50,
-        color:"white"
-    },
-    forgotAndSignUpText:{
-        color:"white",
-        fontSize:11
-    },
-    loginBtn:{
-        width:"80%",
-        backgroundColor:"#fb5b5a",
-        borderRadius:25,
-        height:50,
-        alignItems:"center",
-        justifyContent:"center",
-        marginTop:40,
-        marginBottom:10
-    },
+   heading{
+       fontSize: 1.5rem,
+       textAlign: "center",
+
+   },
 });
 export default Homepage;
